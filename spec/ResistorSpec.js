6522279529e35f8bd6a8745cd5c43c8d8bb6ed23
Joe Karayusuf <jkarayusuf@gmail.com>
2012-06-13 18:46:47 -0400

    FirstBand defaults value to 0

--- spec/ResistorSpec.js
+++ spec/ResistorSpec.js
@@ -1,8 +1,16 @@
-describe("A Resistor", function() {
+describe("A Four Band Resistor", function() {
   var resistor;
 
   beforeEach(function() {
     resistor = new Resistor(4);
+  });
+
+  describe("Initialize", function() {
+
+    it("sets the firstBandValue 0", function() {
+      expect(resistor.firstBandValue).toEqual(0);
+    });
+
   });
 
   it("has a default value of 0", function() {
--- src/resistor.js
+++ src/resistor.js
@@ -7,6 +7,7 @@ function Resistor(numberOfBands) {
   var resistor = {};
 
   resistor.numberOfBands = numberOfBands;
+  resistor.firstBandValue = 0;
   resistor.value = 0;
 
   return resistor;
