c70a255a147b3cd95871f0e95c512b08ce4bf77d
Joe Karayusuf <jkarayusuf@gmail.com>
2012-06-13 18:58:30 -0400

    Set the initial values

--- spec/ResistorSpec.js
+++ spec/ResistorSpec.js
@@ -7,14 +7,26 @@ describe("A Four Band Resistor", function() {
 
   describe("Initialize", function() {
 
-    it("sets the firstBandValue 0", function() {
+    it("sets the firstBandValue to 0", function() {
       expect(resistor.firstBandValue).toEqual(0);
     });
 
-  });
+    it("sets the secondBandValue to 0", function() {
+      expect(resistor.secondBandValue).toEqual(0);
+    });
 
-  it("has a default value of 0", function() {
-    expect(resistor.value).toEqual(0);
+    it("sets the multiplierValue to 1", function() {
+      expect(resistor.multiplierValue).toEqual(1);
+    });
+
+    it("sets the toleranceValue to 5", function() {
+      expect(resistor.toleranceValue).toEqual(5);
+    });
+
+    it("sets the value to 0", function() {
+      expect(resistor.value).toEqual(0);
+    });
+
   });
 
 });
--- src/resistor.js
+++ src/resistor.js
@@ -8,6 +8,9 @@ function Resistor(numberOfBands) {
 
   resistor.numberOfBands = numberOfBands;
   resistor.firstBandValue = 0;
+  resistor.secondBandValue = 0;
+  resistor.multiplierValue = 1;
+  resistor.toleranceValue = 5;
   resistor.value = 0;
 
   return resistor;
