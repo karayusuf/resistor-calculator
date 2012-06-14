92d71db62bec11de992bf0dd6fd0b538660098b9
Joe Karayusuf <jkarayusuf@gmail.com>
2012-06-13 22:37:32 -0400

    Resistor now has bands on the canvas

--- src/ResistorPresenter.js
+++ src/ResistorPresenter.js
@@ -5,12 +5,15 @@ function ResistorPresenter(canvas) {
 
   function drawWire() {
     var wire = canvas.rect(50, 50, 600, 4);
-    wire.attr({ 'stroke' : '#000', 'stroke-width' : '2' });
+    wire.attr({
+      'fill'         : '#fff'
+    , 'stroke'       : '#000'
+    , 'stroke-width' : '2' });
     return wire;
   };
 
   function drawBody() {
-    var body = canvas.rect(252, 22, 200, 58);
+    var body = canvas.rect(250, 22, 200, 58);
     body.attr({
       'fill'         : '#fff'
     , 'stroke'       : '#000'
@@ -19,9 +22,24 @@ function ResistorPresenter(canvas) {
     return body;
   };
 
+  function drawBand(number) {
+    var x = 250 + (10 * (number + number));
+    var band = canvas.rect(x, 22, 10, 58);
+    band.attr({
+      'fill'         : '#d2d2d2'
+    , 'stroke'       : '#000'
+    , 'stroke-width' : '2'
+    });
+    return band;
+  };
+
   presenter.draw = function() {
     presenter.wire = drawWire();
     presenter.body = drawBody();
+    presenter.firstBand = drawBand(1);
+    presenter.secondBand = drawBand(2);
+    presenter.multiplierBand = drawBand(4);
+    presenter.toleranceBand = drawBand(8);
   };
 
   return presenter;
--- src/application.js
+++ src/application.js
@@ -3,6 +3,6 @@
   var canvas = Raphael("paper", 700, 800);
   var resistor = new ResistorPresenter(canvas);
 
-  resistor.draw(50, 50, 600, 100);
+  resistor.draw();
 
 })();
