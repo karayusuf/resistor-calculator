8941e0cb070196b3e6062ce9c83390344f57cefa
Joe Karayusuf <jkarayusuf@gmail.com>
2012-06-14 21:52:04 -0400

    Reset application

--- src/application.js
+++ src/application.js
@@ -2,19 +2,4 @@
 
   var canvas = Raphael("paper", 700, 800);
 
-  var resistor = new ResistorPresenter(canvas);
-  resistor.draw();
-
-  var firstBandSelect = new BandValueSelect(resistor.firstBand);
-  firstBandSelect.draw(canvas, 50);
-
-  var secondBandSelect = new BandValueSelect(resistor.secondBand);
-  secondBandSelect.draw(canvas, 170);
-
-  var multiplierBandSelect = new MultiplierBandSelect(resistor.multiplierBand);
-  multiplierBandSelect.draw(canvas, 290);
-
-  var toleranceBandSelect = new BandValueSelect(resistor.toleranceBand);
-  toleranceBandSelect.draw(canvas, 410);
-
 })();
